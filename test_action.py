f9abc1d65cf831e8d3f83f180b4150a38baec248
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2023-01-02 21:40:05 +0200

    BREAKING CHANGE: create superuser
    The 'setup-postgres' action used to create a superuser that hasn't been
    exposed to users via 'connection-uri' output. The superuser has been
    named after GitHub Action's system user (i.e. 'runner'), had no password
    and could have been used via PostgreSQL client applications [1] or when
    using manually constructed connection URI with no user set.
    
    The user set via action's input parameters used to be unprivileged with
    escalated permissions to create databases on-demand. I don't remember
    why I made things this way, maybe I got confused somewhere along the
    way, but I don't think having both private superuser and public
    unprivileged user is a good idea. It's quite common in tests to
    dynamically create databases and/or users for applications under test,
    thus superuser permissions are required.
    
    This patch removes a private superuser named after the GitHub Action's
    system user (i.e. 'runner') in favor of granting superuser permissions
    to a user set via action's input parameters. Those who explicitly relied
    on 'runner' user might got affected as the user WON'T exist anymore.
    
    [1] https://www.postgresql.org/docs/15/reference-client.html

--- test_action.py
+++ test_action.py
@@ -1,20 +1,40 @@
-import typing as t
+import locale
 import os
+import subprocess
+import typing as t
 
 import psycopg
+import furl
 import pytest
 
 
+ConnectionFactory = t.Callable[[str], psycopg.Connection]
+
+
 @pytest.fixture(scope="function")
-def connection_factory() -> t.Callable[[str], psycopg.Connection]:
+def connection_uri() -> str:
+    """Read and return connection URI from environment."""
+
+    connection_uri = os.getenv("CONNECTION_URI")
+    if connection_uri is None:
+        pytest.fail("CONNECTION_URI: environment variable is not set")
+    return connection_uri
+
+
+@pytest.fixture(scope="function")
+def connection_factory() -> ConnectionFactory:
+    """Return 'psycopg.Connection' factory."""
+
     def factory(connection_uri: str) -> psycopg.Connection:
         return psycopg.connect(connection_uri)
     return factory
 
 
 @pytest.fixture(scope="function")
-def connection(connection_factory) -> psycopg.Connection:
-    return connection_factory(os.getenv("CONNECTION_URI"))
+def connection(connection_uri: str, connection_factory: ConnectionFactory) -> psycopg.Connection:
+    """Return 'psycopg.Connection' for connection URI set in environment."""
+
+    return connection_factory(connection_uri)
 
 
 def test_connection_uri():
@@ -34,12 +54,15 @@ def test_server_encoding(connection: psycopg.Connection):
 def test_locale(connection: psycopg.Connection):
     """Test that PostgreSQL's locale is 'en_US.UTF-8'."""
 
-    assert connection.execute("SHOW LC_COLLATE").fetchone()[0] == "en_US.UTF-8"
-    assert connection.execute("SHOW LC_CTYPE").fetchone()[0] == "en_US.UTF-8"
+    lc_collate = connection.execute("SHOW LC_COLLATE").fetchone()[0]
+    lc_ctype = connection.execute("SHOW LC_CTYPE").fetchone()[0]
+
+    assert locale.normalize(lc_collate) == "en_US.UTF-8"
+    assert locale.normalize(lc_ctype) == "en_US.UTF-8"
 
 
 def test_user_permissions(connection: psycopg.Connection):
-    """Test that a user can create databases but is not a superuser."""
+    """Test that a user has super/createdb permissions."""
 
     with connection:
         record = connection \
@@ -49,7 +72,7 @@ def test_user_permissions(connection: psycopg.Connection):
 
         usecreatedb, usesuper = record
         assert usecreatedb
-        assert not usesuper
+        assert usesuper
 
 
 def test_user_create_insert_select(connection: psycopg.Connection):
@@ -82,12 +105,65 @@ def test_user_create_insert_non_ascii(connection: psycopg.Connection):
 
 
 def test_user_create_drop_database(connection: psycopg.Connection):
-    """Test that a user has no permissions to create databases."""
+    """Test that a user has permissions to create databases."""
 
     # CREATE/DROP DATABASE statements don't work within transactions, and with
     # autocommit disabled transactions are created by psycopg automatically.
     connection.autocommit = True
 
-    database_name = "foobar42"
-    connection.execute(f"CREATE DATABASE {database_name}")
-    connection.execute(f"DROP DATABASE {database_name}")
+    database = "databas3"
+    connection.execute(f"CREATE DATABASE {database}")
+    connection.execute(f"DROP DATABASE {database}")
+
+
+def test_user_create_drop_user(
+    connection: psycopg.Connection,
+    connection_factory: ConnectionFactory,
+    connection_uri: str
+):
+    """Test that a user has permissions to create users."""
+
+    # CREATE/DROP USER statements don't work within transactions, and with
+    # autocommit disabled transactions are created by psycopg automatically.
+    connection.autocommit = True
+
+    username = "us3rname"
+    password = "passw0rd"
+    database = "databas3"
+
+    connection.execute(f"CREATE USER {username} WITH PASSWORD '{password}'")
+    connection.execute(f"CREATE DATABASE {database} WITH OWNER '{username}'")
+
+    try:
+        # Smoke test that created user can successfully log-in and execute
+        # queries for its own database.
+        connection_uri = furl.furl(
+            connection_uri, username=username, password=password, path=database).url
+        test_user_create_insert_select(connection_factory(connection_uri))
+
+    finally:
+        connection.execute(f"DROP DATABASE {database}")
+        connection.execute(f"DROP USER {username}")
+
+
+def test_client_applications(connection_uri, connection_factory):
+    """Test that PostgreSQL client applications can be used."""
+
+    username = "us3rname"
+    password = "passw0rd"
+    database = "databas3"
+
+    subprocess.check_call(["createuser", username])
+    subprocess.check_call(["createdb", "--owner", username, database])
+    subprocess.check_call(["psql", "-c", f"ALTER USER {username} WITH PASSWORD '{password}'"])
+
+    try:
+        # Smoke test that created user can successfully log-in and execute
+        # queries for its own database.
+        connection_uri = furl.furl(
+            connection_uri, username=username, password=password, path=database).url
+        test_user_create_insert_select(connection_factory(connection_uri))
+
+    finally:
+        subprocess.check_call(["dropdb", database])
+        subprocess.check_call(["dropuser", username])
